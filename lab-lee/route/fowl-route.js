f9eb1cbec31ee6af98822c8bd05cdc7076d0451d
Lee Broxson <broxsonl@gmail.com>
2016-09-28 14:29:59 -0700

    Tests written.

--- lab-lee/route/fowl-route.js
+++ lab-lee/route/fowl-route.js
@@ -2,13 +2,16 @@
 
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
+const debug = require('debug')('fowl:router');
+const createError = require('http-errors');
 
-const Fowl = require('../model/fowl.js')
+const Fowl = require('../model/fowl.js');
 
 const fowlRouter = module.exports = new Router();
 
 
 fowlRouter.post('/api/fowl', jsonParser, function(req, res, next) {
+  debug('hit route GET');
   req.body.timestamp = new Date();
   new Fowl(req.body).save()
   .then(fowl => res.json(fowl))
@@ -16,7 +19,22 @@ fowlRouter.post('/api/fowl', jsonParser, function(req, res, next) {
 });
 
 fowlRouter.get('/api/fowl/:id', function(req, res, next) {
+  debug('hit route GET');
   Fowl.findById(req.params.id)
   .then(fowl => res.json(fowl))
-  .catch(next);
+  .catch(err => next(createError(404, err.message)));
+});
+
+fowlRouter.delete('/api/fowl/:id', function(req, res, next) {
+  debug('hit route DELETE /api/fowl');
+  Fowl.deleteFowl(req.params.id)
+  .then( () => res.sendStatus(204))
+  .catch(err => next(createError(404, err.message)));
+});
+
+fowlRouter.put('/api/fowl/:id', jsonParser, function(req, res, next) {
+  debug('hit route PUT /api/fowl/:id');
+  Fowl.updateFowl(req.params.id, req.body)
+  .then(fowl => res.json(fowl))
+  .catch(err => next(createError(404, err.message)));
 });
--- lab-lee/test/fowl-route-test.js
+++ lab-lee/test/fowl-route-test.js
@@ -3,7 +3,6 @@
 const PORT = process.env.PORT|| 3000;
 process.env.MONGODB_URI = 'mongodb://localhost/fowltest';
 
-const debug = require('debug');
 const expect = require('chai').expect;
 const request = require('superagent');
 const Fowl = require('../model/fowl.js');
@@ -17,11 +16,11 @@ const exampleFowl = {
 
 describe('testing route /api/note', function() {
 
-  describe('testing POST requests', function() {
+  describe('testing POST /api/duck requests', function() {
 
     describe('with valid body', function() {
 
-      after( done=> {
+      after( done => {
         if(this.tempFowl) {
           Fowl.remove({})
           .then(() => done())
@@ -30,8 +29,7 @@ describe('testing route /api/note', function() {
         }
       });
 
-
-      it('should return a fowl', done => {
+      it('should return a fowl and status 200', done => {
         request.post(`${url}/api/fowl`)
         .send(exampleFowl)
         .end((err, res) => {
@@ -43,7 +41,20 @@ describe('testing route /api/note', function() {
         });
       });
     });
+
+    describe('with an invalid body', function() {
+
+      it('should return 400 bad request', done => {
+        request.post(`${url}/api/fowl`)
+        .send({name: ''})
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          done();
+        });
+      });
+    });
   });
+
   describe('testing GET requests', function() {
 
     describe('with valid body', function() {
@@ -76,6 +87,117 @@ describe('testing route /api/note', function() {
           expect(res.status).to.equal(200);
           expect(res.body.name).to.equal('Jeff');
           this.tempFowl = res.body;
+          done();
+        });
+      });
+    });
+
+    describe('with a file that doesn\'t exist', function() {
+
+      it('should give a 404 invalid id', (done => {
+        request.get(`${url}/api/fowl/nope-duck`)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      }));
+    });
+
+  });
+
+  describe('Testing PUT /api/fowl:id requests', function() {
+
+    describe('with valid id', function() {
+
+      before( done => {
+        exampleFowl.timestamp = new Date();
+        new Fowl(exampleFowl).save()
+        .then( fowl => {
+          this.tempFowl = fowl;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        delete exampleFowl.timestamp;
+        if(this.tempFowl) {
+          Fowl.remove({})
+          .then(() => done())
+          .catch(done);
+          return;
+        }
+        done();
+      });
+
+      it('should return a fowl with a status 200', done => {
+        let updateData = {name:'steve'};
+        request.put(`${url}/api/fowl/${this.tempFowl._id}`)
+        .send(updateData)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('steve');
+          this.tempFowl = res.body;
+          done();
+        });
+      });
+    });
+
+    describe('with no body provided', function() {
+
+      it('should return a 400 bad request', done => {
+        request.put(`${url}/api/fowl/${this.tempFowl._id}`)
+        .send({fur: 'stuff'})
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          done();
+        });
+      });
+    });
+
+    describe('with a body that doesn\'t exist', function() {
+
+      it('should 404 not found', done => {
+        request.put(`${url}/api/fowl/nope-duck`)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('Testing DELETE /api/fowl:id requests', function() {
+
+    describe('with valid id', function() {
+
+      before( done => {
+        exampleFowl.timestamp = new Date();
+        new Fowl(exampleFowl).save()
+        .then( fowl => {
+          this.tempFowl = fowl;
+          done();
+        })
+        .catch(done);
+      });
+
+      it('should delete a fowl with a status 204', done => {
+        request.delete(`${url}/api/fowl/${this.tempFowl._id}`)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(204);
+          done();
+        });
+      });
+    });
+
+    describe('with an id that doesn\'t exist', function() {
+
+      it('should 404 not found', done => {
+        request.delete(`${url}/api/fowl/nope-duck`)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
           done();
         });
       });
