db6ab39b2059053f49f3e917c6e0c95d9f29f400
kaylynyuh <yuhkay4@gmail.com>
2016-09-28 13:41:32 -0700

    GET and POST tests for status codes of 200 passing

--- lab-kaylyn/test/portfolio-route-test.js
+++ lab-kaylyn/test/portfolio-route-test.js
@@ -2,7 +2,7 @@
 'use strict';
 
 const PORT = process.env.PORT || 3000;
-process.env.MONGODB_URI = 'mongodb://localhost/notetest';
+process.env.MONGODB_URI = 'mongodb://localhost/portfoliotest';
 
 const expect = require('chai').expect;
 const request = require('superagent');
@@ -12,7 +12,10 @@ require('../server.js');
 
 const url = `http://localhost:${PORT}`;
 const examplePortfolio = {
-  name: 'lulwat',
+  name: 'pudge',
+  about: 'about pudge',
+  projects: 'projects of pudge',
+  work: 'pudge work',
 };
 
 describe('testing route /api/portfolio', function(){
@@ -34,7 +37,10 @@ describe('testing route /api/portfolio', function(){
         .end((err, res) => {
           if (err) return done(err);
           expect(res.status).to.equal(200);
-          expect(res.body.name).to.equal('lulwat');
+          expect(res.body.name).to.equal('pudge');
+          expect(res.body.about).to.equal('about pudge');
+          expect(res.body.projects).to.equal('projects of pudge');
+          expect(res.body.work).to.equal('pudge work');
           this.tempPortfolio = res.body;
           done();
         });
@@ -70,7 +76,10 @@ describe('testing route /api/portfolio', function(){
         .end((err, res) => {
           if (err) return done(err);
           expect(res.status).to.equal(200);
-          expect(res.body.name).to.equal('lulwat');
+          expect(res.body.name).to.equal('pudge');
+          expect(res.body.about).to.equal('about pudge');
+          expect(res.body.projects).to.equal('projects of pudge');
+          expect(res.body.work).to.equal('pudge work');
           done();
         });
       });
