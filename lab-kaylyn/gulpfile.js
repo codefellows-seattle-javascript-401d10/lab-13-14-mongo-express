f54158bfcefea4189bdd7947ca2f727db92196e6
kaylynyuh <yuhkay4@gmail.com>
2016-09-28 13:02:03 -0700

    init commit

---
+++ lab-kaylyn/gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const gulp = require('gulp');
+const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+
+gulp.task('test', function(){
+  gulp.src('./test/*-test.js', {read:true})
+  .pipe(mocha({reporter:'nyan'}));
+});
+
+gulp.task('eslint', function(){
+  return gulp.src(['**/*.js','!node_modules/**'])
+  .pipe(eslint())
+  .pipe(eslint.format())
+  .pipe(eslint.failAfterError());
+});
+
+gulp.task('watch', function(){
+  gulp.watch(['./*.js','**/*.js','./lib/*.js'],['eslint','test']);
+});
+
+gulp.task('default',['watch']);
