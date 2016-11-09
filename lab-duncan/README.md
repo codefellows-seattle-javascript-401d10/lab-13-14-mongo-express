651e3c6fc91cc7ab76367bae2b779cf5af3d00f7
Duncan Marsh <dwolfm@gmail.com>
2016-11-09 14:18:46 -0800

    passing example test

---
+++ lab-duncan/README.md
@@ -0,0 +1,4 @@
+# lab 13
+
+guna write an express server   
+la ti da  
---
+++ lab-duncan/test/lulwat-test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe('true', function(){
+  it('shold pass', () => {
+    expect(false).to.equal(false);
+  });
+});
